1076160e7b3e3b07b58bfabae8e3c336858da7a3
Marco  Iannuzzi <92125086+Marcoiannuzzi@users.noreply.github.com>
2022-10-27 20:31:13 -0300

    sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,8 @@ import { AgregarEditarMascotaComponent } from './components/agregar-editar-masco
 import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
 import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// MATERIAL
-import { MatSliderModule } from '@angular/material/slider';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -26,12 +21,7 @@ import {MatInputModule} from '@angular/material/input';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
